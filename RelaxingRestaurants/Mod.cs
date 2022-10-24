1335369f154553b053997340a3c135978ab97193
Lachlan Leone <starfluxgames@gmail.com>
2022-10-24 12:59:06 +1100

    v0.1.0

---
+++ RelaxingRestaurants/Mod.cs
@@ -0,0 +1,38 @@
+﻿using KitchenLib;
+using HarmonyLib;
+using UnityEngine;
+using MelonLoader;
+using Kitchen;
+using KitchenData;
+using Unity.Collections;
+
+namespace RelaxingRestaurants
+{
+    public class Mod : BaseMod
+    {
+        public Mod() : base("relaxingrestaurants", "1.0.5") {}
+
+        public override void OnInitializeMelon()
+        {
+        }
+
+        [HarmonyPatch(typeof(UpdateQueuePatience), "OnUpdate")]
+        class UpdateQueuePatience_Patch
+        {
+            public static bool Prefix()
+            {
+                return false;
+		    }
+        }
+
+        [HarmonyPatch(typeof(CCustomerSettings), "AddPatience")]
+        class CCustomerSettings_Patch
+        {
+            public static void Postfix(ref CPatience patience)
+            {
+                patience.StartTime = 999999999f;
+                patience.RemainingTime = 999999999f;
+		    }
+        }
+    }
+}
---
+++ RelaxingRestaurants/Properties/AssemblyInfo.cs
@@ -0,0 +1,4 @@
+using MelonLoader;
+
+[assembly: MelonInfo(typeof(RelaxingRestaurants.Mod), "Relaxing Restaurants", "0.1.0", "StarFluxGames")]
+[assembly: MelonGame("It's Happening", "PlateUp")]
